21336e5a200a5571c58ac3487965b8f2a864accb
Arafat Kabir Khan <arafatkabir@iut-dhaka.edu>
2025-02-06 04:01:49 +0600

    added sort functionality

--- scripts/video.js
+++ scripts/video.js
@@ -128,13 +128,23 @@ const removeActiveButtons = () =>{
 }
 
 const sortVideos = async ()=>{
-		const res = await fetch("https://openapi.programming-hero.com/api/phero-tube/videos?title=")
+		const activeCategoryID = findActiveCategory()
+		let res;
+		if (activeCategoryID){
+			res = await fetch(`https://openapi.programming-hero.com/api/phero-tube/category/${activeCategoryID}`)
+		}
+		else{
+			res = await fetch("https://openapi.programming-hero.com/api/phero-tube/videos?title=")
+		}
 		const data = await res.json()
-		const videos = data.videos
+		const videos = data.videos ? data.videos : data.category
 
 		videos.sort((a, b) => parseViews(b.others.views) - parseViews(a.others.views))
 		displayVideos(videos)
-
+}
+const findActiveCategory = () =>{
+	const activeCategory = document.getElementsByClassName("active-button")
+	if (activeCategory.length) return activeCategory[0].id.slice(16)
 }
 const parseViews = (views) => {
 	if (views.endsWith("k")) {
